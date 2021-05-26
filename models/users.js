ee28c3c1370cdf349f87701f1882517201540ce1
Gaurav Deep <gauravdeep2151@gmail.com>
2021-05-26 19:18:09 +0530

    Uploading Profile Picture

--- models/users.js
+++ models/users.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-
+const multer = require('multer');
+const path = require('path');
+const AVATAR_PATH  = path.join('/uploads/users/avatars');
 const userSchema = new mongoose.Schema({
   email: {
       type: String,
@@ -10,11 +12,29 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,  
     },
-    name: {
-        type: String,
-        required: true,
-    }
+  name: {
+      type: String,
+      required: true,
+  },
+  avatar: {
+    type: String
+
+  }
 },{timestamps : true,});
+
+let storage = multer.diskStorage({
+  destination: function(req,file,cb){
+    cb(null,path.join(__dirname,'..',AVATAR_PATH))
+  },
+  filename: function(req,file,cb){
+    console.log(file);
+    cb(null,file.fieldname+'-'+req.user.id);
+  }
+});
+
+//STATIC_FUNCTIONS
+userSchema.statics.uploadedAvatar = multer({storage: storage}).single('avatar');
+userSchema.statics.avatarPath = AVATAR_PATH;
 
 const User = mongoose.model('User',userSchema);
 module.exports = User;
